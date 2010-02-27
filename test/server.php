b8e2311f3634975a3a2d551bbe84b18147744fb3
okonomi <okonomi@224e63c0-5457-0410-989b-907d9cf58824>
2010-02-27 12:23:06 +0000

    HTTP_Downloadの読み込みが抜けてる
    git-svn-id: http://svn.openpear.org/HTTP_Download_Mobile_EZget/trunk@1675 224e63c0-5457-0410-989b-907d9cf58824

--- test/server.php
+++ test/server.php
@@ -2,6 +2,7 @@
 
 require_once dirname(__FILE__).'/bootstrap.php';
 require_once dirname(__FILE__).'/../src/HTTP/Download/Mobile/EZget.php';
+require_once 'HTTP/Download.php';
 
 
 $ezget = new HTTP_Download_Mobile_EZget();
