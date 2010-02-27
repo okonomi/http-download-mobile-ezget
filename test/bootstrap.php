9bfa278d858fa24a6d30731682c8028a45a56b2f
okonomi <okonomi@224e63c0-5457-0410-989b-907d9cf58824>
2010-02-27 12:19:28 +0000

    テストを整理
    git-svn-id: http://svn.openpear.org/HTTP_Download_Mobile_EZget/trunk@1660 224e63c0-5457-0410-989b-907d9cf58824

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,7 +1,17 @@
 <?php
 
-error_reporting(E_ALL);
-set_include_path(dirname(__FILE__)."/../src" . PATH_SEPARATOR . get_include_path());
+
+mb_language('ja');
+mb_internal_encoding('UTF-8');
+
+if (defined('E_DEPRECATED')) {
+    error_reporting(E_ALL & ~E_DEPRECATED);
+} else {
+    error_reporting(E_ALL);
+}
+
+set_include_path(realpath(dirname(__FILE__) . '/../src') . PATH_SEPARATOR .
+                 get_include_path());
 
 
 define ('HTTP_DOWNLOAD_MOBILE_EZGET_DATA_DIR', dirname(__FILE__).'/data');
